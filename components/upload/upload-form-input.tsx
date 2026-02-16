3d00d347f4cbb5da0c3c4d867147957be4287513
Nima Azadbar <nima.azb.5@gmail.com>
2026-02-16 23:15:37 +0330

    upload page form done. zod installed

---
+++ components/upload/upload-form-input.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+interface UploadFormInputProps {
+  onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+}
+
+export default function UploadFormInput({ onSubmit }: UploadFormInputProps) {
+  return (
+    <form className="flex flex-col gap-6" onSubmit={onSubmit}>
+      <div className="flex jstify-end items-center gap-1.5">
+        <Input
+          id="file"
+          name="file"
+          type="file"
+          accept="application/pdf"
+          required
+          className=""
+        />
+        <Button>Upload your PDF</Button>
+      </div>
+    </form>
+  );
+}
--- components/upload/upload-form.tsx
+++ components/upload/upload-form.tsx
@@ -1,3 +1,25 @@
+"use client";
+
+import UploadFormInput from "./upload-form-input";
+
 export default function UploadForm() {
-  return <section></section>;
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const file = formData.get("file") as File;
+
+    // validating the fields
+    //schema with zod
+    //upload the file to uploadthing
+    //parse the PDF using lang chain
+    //summarize the PDF using AI
+    //save the summary to the database
+    //redirect to the [id] summary page
+  };
+
+  return (
+    <div className="flex flex-col gap-8 w-full max-w-2xl mx-auto">
+      <UploadFormInput onSubmit={handleSubmit} />
+    </div>
+  );
 }
