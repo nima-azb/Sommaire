e8a82f07ac4de1dddc538870404fd3f5bcdf7a22
Nima Azadbar <nima.azb.5@gmail.com>
2026-02-17 21:42:00 +0330

    setting up uploadthing and API routes

--- components/upload/upload-form.tsx
+++ components/upload/upload-form.tsx
@@ -1,16 +1,77 @@
 "use client";
 
+import { z } from "zod";
 import UploadFormInput from "./upload-form-input";
+import { useUploadThing } from "@/utils/uploadthing";
+import { toast } from "sonner";
+
+const schema = z.object({
+  file: z
+    .instanceof(File, { message: "Invalid file" })
+    .refine(
+      (file) => file.size <= 20 * 1024 * 1024,
+      "File size must be less than 20MB",
+    )
+    .refine(
+      (file) => file.type.startsWith("application/pdf"),
+      "File must be a PDF",
+    ),
+});
 
 export default function UploadForm() {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const { startUpload, routeConfig } = useUploadThing("pdfUploader", {
+    onClientUploadComplete: () => {
+      console.log("uploaded successfully!");
+    },
+    onUploadError: (err) => {
+      console.error("error occurred while uploading", err);
+      toast(" Error occurred while uploading", {
+        description: err.message,
+      });
+    },
+    onUploadBegin: ({ file }) => {
+      console.log("upload has begun for", file);
+    },
+  });
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
     const file = formData.get("file") as File;
 
     // validating the fields
-    //schema with zod
+    const validatedFields = schema.safeParse({ file });
+
+    console.log(validatedFields);
+
+    if (!validatedFields.success) {
+      toast("❌ Something went wrong", {
+        description:
+          validatedFields.error.flatten().fieldErrors.file?.[0] ??
+          "Invalid file.",
+        style: { color: "red" },
+      });
+      return;
+    }
+
+    toast("📄 Uploading PDF...", {
+      description: "We are uploading your PDF!",
+    });
     //upload the file to uploadthing
+
+    const resp = await startUpload([file]);
+    if (!resp) {
+      toast("Something went wrong", {
+        description: "Please use a different file",
+        style: { color: "red" },
+      });
+      return;
+    }
+
+    toast("⏳ Processing PDF...", {
+      description: "Hang tight! Our AI is reading through your document! ✨",
+    });
+
     //parse the PDF using lang chain
     //summarize the PDF using AI
     //save the summary to the database
